0684c8db0b175bea573d787ee1e7bba6abf36dec
2016110441 <jhbravo1997@gamil.com>
2018-12-08 15:03:20 +0900

    first commit

---
+++ app/src/main/java/com/example/jhbra/android_project/SplashAct.java
@@ -0,0 +1,13 @@
+package com.example.jhbra.android_project;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SplashAct extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+    }
+}
