ba4892751090d2b0c352e484afe1dc67d4c854e6
s2016110441 <jhbravo1997@gmail.com>
2018-12-11 22:05:41 +0900

    1

--- app/src/main/java/com/example/jhbra/android_project/SplashAct.java
+++ app/src/main/java/com/example/jhbra/android_project/SplashAct.java
@@ -1,14 +1,84 @@
 package com.example.jhbra.android_project;
 
+
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.AsyncTask;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.CalendarView;
+import android.widget.Toast;
 
 public class SplashAct extends AppCompatActivity {
+
+    static final Integer APP_PERMISSION = 1;
+
+    private void replaceWithLocationAct() {
+        Intent intent = new Intent(this, CalendarView.class);
+        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_TASK_ON_HOME);
+        startActivity(intent);
+        this.finish();
+    }
+
+    private boolean askForPermission(String permission, Integer requestCode) {
+        if (ContextCompat.checkSelfPermission(SplashAct.this, permission)
+                != PackageManager.PERMISSION_GRANTED) {
+            if (ActivityCompat.shouldShowRequestPermissionRationale(
+                    SplashAct.this, permission)) {
+                ActivityCompat.requestPermissions(SplashAct.this,
+                        new String[]{permission}, requestCode);
+            } else {
+                ActivityCompat.requestPermissions(SplashAct.this,
+                        new String[]{permission}, requestCode);
+            }
+            return false;
+        } else {
+            return true;
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions,
+                                           @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        if (ActivityCompat.checkSelfPermission(this, permissions[0])
+                == PackageManager.PERMISSION_GRANTED) {
+            Toast.makeText(this, "Permission granted", Toast.LENGTH_SHORT).show();
+            replaceWithLocationAct();
+        } else {
+            Toast.makeText(this, "Permission denied", Toast.LENGTH_SHORT).show();
+        }
+    }
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+
+        AsyncTask task = new AsyncTask() {
+            @Override
+            protected Object doInBackground(Object[] objects) {
+                try {
+                    Thread.sleep(750);
+                } catch (InterruptedException e) {
+                }
+                boolean alreadyGranted = askForPermission(Manifest.permission.ACCESS_FINE_LOCATION,
+                        APP_PERMISSION);
+                if (alreadyGranted) {
+                    replaceWithLocationAct();
+                }
+                return null;
+            }
+        };
+
+        task.execute();
     }
-    //
+
+
 }
